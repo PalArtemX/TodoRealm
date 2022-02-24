1c5624e95795f8de672f2324f2312bf2b9b1be41
Artem Paliutin <pal.artem@icloud.com>
2022-02-24 13:52:06 +0300

    start

---
+++ TodoRealm/Views/HomeView.swift
@@ -0,0 +1,29 @@
+//
+//  HomeView.swift
+//  TodoRealm
+//
+//  Created by Artem Paliutin on 24.02.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
