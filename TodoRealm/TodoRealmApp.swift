e38183fbb7a1acf81a1a7d4e7d9f0447e2c379d0
Artem Paliutin <pal.artem@icloud.com>
2022-02-24 13:49:15 +0300

    Initial Commit

---
+++ TodoRealm/TodoRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoRealmApp.swift
+//  TodoRealm
+//
+//  Created by Artem Paliutin on 24.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
